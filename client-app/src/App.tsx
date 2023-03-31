e340b3fc3f048a303a3b02fd946ae9b499e04fa2
pspanoudakis <pavlos.spanoudakis@gmail.com>
2023-03-31 03:49:19 +0300

    Messing around with endpoints & jwt's

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,44 +1,70 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-const sampleResponseText = JSON.stringify({a: 1, b: {c:3}}, null, "\t");
-const jwt = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY4MDEyNzUxOSwiZXhwIjoxNjgwMTI3NTc5fQ.rnXUhkvevMYlSV_uLCF5S1jIzmw0W3Sq2udo8dtt7hw';
+// const sampleRequestText = JSON.stringify({"username":"pavlos", "password":"pavlos"}, null, "\t");
 
 export function App() {
 	const DOMAIN_URL = 'http://localhost:8080/';
+	enum HTTP_METHOD {
+		GET = "GET",
+		POST = "POST"
+	};
+
+	const [method, setMethod] = useState(HTTP_METHOD.GET);
+	const [body, setBody] = useState('');
 	const [url, setUrl] = useState(DOMAIN_URL);
 	const [response, setResponse] = useState<string | null>(null);
+	const [jwt, setJwt] = useState('');
 
 	const sendRequest = () => {
 		fetch(
 			url,
 			{
-				// method: 'GET',
-				method: 'POST',
-				body: JSON.stringify({
-					"username": "admin",
-					"password": "admin"
-				}),
+				method: method,
+				body: (
+					method === HTTP_METHOD.GET 
+					?
+					undefined
+					:
+					body
+				), 
 				headers: {
                     'Content-Type': 'application/json',
-					// 'Authorization': `Bearer ${jwt}`
+					'Authorization': jwt ? `Bearer ${jwt}` : ''
                 },
 			}
-		).then(r => {
-			r.json().then(obj => setResponse(JSON.stringify(obj, null, '\t')));
-			// r.text().then(txt => setResponse(txt));
-		}).catch(err => {
+		)
+		.then(r => {
+			if (r.headers.get("content-type") === "application/json") {
+				r.json().then(
+					obj => setResponse(JSON.stringify(obj, null, '\t'))
+				);
+			}
+			else {
+				r.text().then(txt => setResponse(txt));
+			}
+		})
+		.catch(err => {
 			console.error(err);
 			setResponse(null);
 		});
-	}
+	};
 
 	const updateUrl = (urlTail: string) => setUrl(`${DOMAIN_URL}${urlTail}`);
+
+	const updateMethod = (m: HTTP_METHOD) => setMethod(m)
 
 	return (
 		<div className="h-full flex justify-start items-center flex-col gap-4">
 			<p className='text-3xl'>Test CORS</p>
 			<div className='flex gap-1 justify-center items-center text-xl'>
+				<select className='outline-none rounded-md px-2 py-1 font-mono' onChange={e => updateMethod(e.target.value as HTTP_METHOD)}>
+				{
+					Object.values(HTTP_METHOD).map(
+						(m, i) => <option key={i} value={m}>{m}</option>
+					)
+				}
+				</select>
 				<code>{DOMAIN_URL}</code>
 				<input
 					type="text"
@@ -48,11 +74,39 @@ export function App() {
 				/>
 				<button className='py-1 px-2' onClick={sendRequest}>Send Request</button>
 			</div>
+			<div className='w-full flex gap-2 items-center'>
+				<p className='text-xl'>Bearer: </p>
+				<input
+					type="text"
+					className='bg-black flex-grow outline-none rounded-md px-2 py-1 font-mono'
+					onChange={e => setJwt(e.target.value)}
+					spellCheck='false'
+					value={jwt}
+				/>
+				<button className='py-1 px-2' onClick={ () => setJwt('')} disabled={jwt === ''}>Clear</button>
+			</div>
+			{
+				method === HTTP_METHOD.POST ?
+				<div className='w-full flex flex-col gap-2 items-start'>
+					<p className='text-xl'>Request Body:</p>
+					<textarea
+						className='px-2 py-1 font-mono bg-black w-full min-h-fit resize-none rounded-md outline-none'
+						spellCheck='false'
+						value={body}
+						onChange={e => setBody(e.target.value)}
+					/>
+				</div>
+				:
+				null
+			}
 			{
 				response ?
-				<pre className='bg-black text-left rounded-md px-3 py-2 w-full whitespace-pre-wrap'>
-					{response}
-				</pre>
+				<div className='w-full flex flex-col gap-2 items-start'>
+					<p className='text-xl'>Response:</p>
+					<pre className='bg-black text-left rounded-md px-3 py-2 w-full whitespace-pre-wrap'>
+						{response}
+					</pre>
+				</div>
 				:
 				null
 			}
