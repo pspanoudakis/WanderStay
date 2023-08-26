22ebef7eb4fde606ca5c211d8ebcab8b4c275210
pspanoudakis <pavlos.spanoudakis@gmail.com>
2023-08-26 20:23:54 +0300

    Primary Button component

--- client-app/src/components/PrimaryButton.tsx
+++ client-app/src/components/PrimaryButton.tsx
@@ -1,22 +1,26 @@
 import React, { ReactNode } from "react"
 
 type PrimaryButtonProps = {
-    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void,
+    disabled?: boolean
     children?: ReactNode
 }
 
 export function PrimaryButton({
     onClick,
+    disabled,
     children
 }: PrimaryButtonProps) {
     return (
         <button
             className='
                 rounded-xl px-4 py-0.5
-                bg-main-petrol duration-300 hover:bg-dark-petrol
+                bg-main-petrol duration-300 enabled:hover:bg-dark-petrol
+                disabled:bg-light-petrol
                 text-white font-semibold
             '
             onClick={onClick}
+            disabled={disabled}
         >
             {children}
         </button>
