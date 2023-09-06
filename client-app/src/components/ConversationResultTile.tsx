54cb5ea4553dc47dfdfaebd1d3740af3953ffffb
Pavlos Spanoudakis <pavlos.spanoudakis@gmail.com>
2023-09-06 03:50:08 +0300

    Conversation preview tiles

--- client-app/src/components/ConversationResultTile.tsx
+++ client-app/src/components/ConversationResultTile.tsx
@@ -1,18 +1,55 @@
 import { Link } from "react-router-dom";
 import { PropertyConversationResult } from "../api/responses/PropertyConversationResult";
 import { ORDERED_BASE_ROLE_PATHS } from "../pages/pathConstants";
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+import { Avatar } from "@mui/material";
+
+const MAX_MSG_TEXT_PREVIEW_LENGTH = 80;
 
 export function ConversationResultTile(props: {
     conversationPreview: PropertyConversationResult,
     propertyId: Number
 }) {
+    const {userContext} = useContext(AppContext).state.businessContext;
     const conversation = props.conversationPreview;
     return (
         <Link 
             to={`${ORDERED_BASE_ROLE_PATHS.HOST}/property/${props.propertyId}/chat/${conversation.id}`} 
-            className="flex flex-col gap-3"
+            className="
+                flex justify-center items-center gap-3 
+                border-2 border-main-petrol rounded-xl px-3 py-2
+                duration-200 hover:bg-xlight-petrol
+            "
         >
-            Conversation {conversation.id}
+            <Avatar 
+                className="border-2 border-main-petrol"
+                sx={{
+                    color: 'white',
+                    bgcolor: "primary.main" ,
+                    mt: 0.2
+                }}
+            >
+                {conversation.guestUsername.toUpperCase().charAt(0)}
+            </Avatar>
+            <div className="flex flex-col w-full items-start">
+                <div className="flex w-full items-center justify-between">
+                    <span className="text-lg font-bold">{conversation.guestUsername}</span>
+                    <span className="text-sm">{conversation.lastMessage.sentOn}</span>
+                </div>
+                <span>
+                    {conversation.lastMessage.sentBy === userContext?.username ? 'Εσείς: ' : ''}
+                    <span className="italic">
+                        {
+                            conversation.lastMessage.text.length > MAX_MSG_TEXT_PREVIEW_LENGTH ?
+                            `${conversation.lastMessage.text.slice(0, MAX_MSG_TEXT_PREVIEW_LENGTH - 1)}...`
+                            :
+                            conversation.lastMessage.text
+                        }
+                    </span>
+                </span>
+            </div>
+            
         </Link>
     );
 }
--- client-app/tailwind.config.cjs
+++ client-app/tailwind.config.cjs
@@ -9,7 +9,8 @@ module.exports = {
       colors: {
         'main-petrol': '#6fb69e',
         'dark-petrol': '#408972',
-        'light-petrol': '#b0f1da'
+        'light-petrol': '#b0f1da',
+        'xlight-petrol': '#d4f0e6',
       }
     },
   },
