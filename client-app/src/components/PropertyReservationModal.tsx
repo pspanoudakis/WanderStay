a4e27086b458a0f0b0afc5a9ef57e0befe14a7c0
pspanoudakis <pavlos.spanoudakis@gmail.com>
2023-09-24 18:29:06 +0300

    Select Reservation date range using Property available time slots

--- client-app/src/components/PropertyReservationModal.tsx
+++ client-app/src/components/PropertyReservationModal.tsx
@@ -10,6 +10,10 @@ import { PageTitleSpan } from "./PageTitleSpan";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarAlt, faCheck, faHandshake, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import { PrimaryButton } from "./PrimaryButton";
+import { DateRangePicker, LocalizationProvider } from "@mui/x-date-pickers-pro";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { AvailableTimeSlot } from "../api/entities/AvailableTimeSlot";
+import { dateToStr } from "../api/entities/dates";
 
 export function PropertyReservationModal({ property }: {
     property: PropertyDetails
@@ -20,12 +24,43 @@ export function PropertyReservationModal({ property }: {
 
     const [loading, setLoading] = useState(false);
 
+    const [dateFrom, setDateFrom] = useState(() => dayjs(searchContext.dateFrom));
+    const [selectedSlot, setSelectedSlot] = useState<AvailableTimeSlot | undefined>(
+        () => property.availableSlots.find(
+            slot => (
+                (dateFrom.isAfter(slot.startDate) || dateFrom.isSame(slot.startDate)) &&
+                (dateFrom.isBefore(slot.endDate) || dateFrom.isSame(slot.endDate))
+            )
+        )
+    );
+    const [dateTo, setDateTo] = useState(() => {
+        const tempDayTo = dayjs(searchContext.dateTo);
+        if (
+            tempDayTo.isValid() &&
+            (tempDayTo.isAfter(selectedSlot?.startDate) || tempDayTo.isSame(selectedSlot?.startDate)) &&
+            (tempDayTo.isBefore(selectedSlot?.endDate) || tempDayTo.isSame(selectedSlot?.endDate))
+        ) {
+            return tempDayTo;
+        }
+        return dayjs(null);
+    });
+
+    const dailyCost = useMemo(() => (
+        searchContext.numPersons * property.rules.perGuestCost + property.rules.baseDayCost
+    ), [searchContext.numPersons, property.rules.perGuestCost, property.rules.baseDayCost]);    
+    const durationInDays = useMemo(() => {
+        return Math.ceil(
+            (dateTo.valueOf() - dateFrom.valueOf()) /
+            (1000 * 60 * 60 * 24)
+        );
+    }, [dateTo, dateFrom]);
+
     const makeReservation = () => {
         setLoading(true);
         if (property) {
             makePropertyReservation(property.propertyId, {
-                dateFrom: searchContext.dateFrom ?? '',
-                dateTo: searchContext.dateTo ?? '',
+                dateFrom: dateToStr(dateFrom.toDate()) ?? '',
+                dateTo: dateToStr(dateTo.toDate()) ?? '',
                 numPersons: searchContext.numPersons
             })
             .then(response => {
@@ -43,45 +78,64 @@ export function PropertyReservationModal({ property }: {
             })
         }
     }
-
-    const dailyCost = useMemo(() => (
-        searchContext.numPersons * property.rules.perGuestCost + property.rules.baseDayCost
-    ), [searchContext.numPersons, property.rules.perGuestCost, property.rules.baseDayCost]);    
-    const dateFrom = useMemo(
-        () => dayjs(searchContext.dateFrom), 
-        [searchContext.dateFrom]
-    );
-    const dateTo = useMemo(
-        () => dayjs(searchContext.dateTo),
-        [searchContext.dateTo]
-    );
-    const durationInDays = useMemo(() => {
-        return Math.ceil(
-            (dateTo.valueOf() - dateFrom.valueOf()) /
-            (1000 * 60 * 60 * 24)
-        );
-    }, [dateTo, dateFrom]);
-
-    if (!dateFrom.isValid() || !dateTo.isValid()) {
-        return (
-            <ModalActionResultTemplate
-                success={false}
-                errorText="Παρακαλώ επιλέξτε ένα έγκυρο διάστημα στην μπάρα αναζήτησης."
-            />
-        );
-    }
     
     return (
-        <div className="w-96 flex flex-col justify-center items-center relative gap-5">
+        <div className="w-96 flex flex-col justify-center items-center relative gap-6">
             <LoadingSpinner 
                 coverParent
                 visible={loading}
             />
             <PageTitleSpan>Η Κράτησή σας</PageTitleSpan>
-            <div className="flex flex-col items-center text-lg gap-1">
-                <span className="flex justify-start items-center gap-3">
+            <div className="flex flex-col items-center text-lg gap-4">
+                <span className="flex justify-start items-center gap-2">
                     <FontAwesomeIcon size="lg" icon={faCalendarAlt}/>
-                    {searchContext.dateFrom} έως {searchContext.dateTo}
+                    <LocalizationProvider dateAdapter={AdapterDayjs} >
+                        <DateRangePicker
+                            value={[dateFrom, dateTo]}
+                            onChange={range => {
+                                setSelectedSlot(
+                                    property.availableSlots.find(
+                                        slot => (
+                                            (range[0]?.isAfter(slot.startDate) || range[0]?.isSame(slot.startDate)) &&
+                                            (range[0]?.isBefore(slot.endDate) || range[0]?.isSame(slot.endDate))
+                                        )
+                                    )
+                                );
+                                setDateFrom(dayjs(range[0]));
+                                setDateTo(dayjs(range[1]));
+                            }}
+                            localeText={{ start: 'Check-in', end: 'Check-out' }}
+                            shouldDisableDate={(d, pos) => {
+                                if (pos === 'start') {
+                                    for (const slot of property.availableSlots) {
+                                        if (
+                                            (d.isAfter(slot.startDate) || d.isSame(slot.startDate)) &&
+                                            (d.isBefore(slot.endDate) || d.isSame(slot.endDate))
+                                        ) {
+                                            return false;
+                                        }
+                                    }
+
+                                } else if (
+                                    (pos === 'end') && dateFrom.isValid() && selectedSlot &&
+                                    (d.isAfter(selectedSlot.startDate) || d.isSame(selectedSlot.startDate)) &&
+                                    (d.isBefore(selectedSlot.endDate) || d.isSame(selectedSlot.endDate))
+                                ) {
+                                    return false;
+                                }
+                                return true;
+                            }}
+                            sx={{
+                                width: '18rem',
+                                height: 'max-content',
+                            }}
+                            slotProps={{
+                                textField: {
+                                    size: 'small'
+                                }
+                            }}
+                        />
+                    </LocalizationProvider>
                 </span>
                 <span className="flex justify-start items-center gap-3">
                     <FontAwesomeIcon size="lg" icon={faUserGroup}/>
@@ -98,6 +152,10 @@ export function PropertyReservationModal({ property }: {
             <PrimaryButton 
                 classExtras="flex gap-3 items-center rounded-xl py-2 px-4 text-xl"
                 onClick={makeReservation}
+                disabled={!(
+                    dateFrom.isValid() && dateTo.isValid() &&
+                    dateFrom.isBefore(dateTo)
+                )}
             >
                 <FontAwesomeIcon icon={faCheck}/>
                 Ολοκλήρωση Κράτησης
